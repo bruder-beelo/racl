b95eaec7c2c9ea3ddeebdb3e473d5af9ee0d61e2
bruder-beelo <danny.chakra96@gmail.com>
2026-02-25 11:06:41 -0700

    fixed margin

--- src/screens/ReservationsScreen.tsx
+++ src/screens/ReservationsScreen.tsx
@@ -59,6 +59,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 40,
+    marginTop: -60,
   },
   emptyTitle: {
     fontSize: 24,
